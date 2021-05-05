a265ac89c4bfeb8a5754377d60f4d31a17302e15
silvinavd <silvinavalverde8@gmail.com>
2021-05-05 18:46:36 +0000

    entrega

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -25,41 +25,87 @@ export function Home() {
 	};
 
 	//BUTTONS
+
+	// PLAY/PAUSE STATE
+	let audio = useRef();
+
+	const [isPlaying, setPlaying] = useState(false);
+
 	const playPause = () => {
 		if (audio.current.paused) {
 			audio.current.play();
+
+			setPlaying(true);
 		} else if (!audio.current.paused) {
 			audio.current.pause();
+			setPlaying(false);
 		}
 	};
-	let audio = useRef();
+
+	//CAMBIAR CANCION
+	const [songActual, setSongActual] = useState();
+
+	const prevSong = () => {
+		let prev = songActual - 1;
+		if (prev <= 0) {
+			prev = songList.length - 1;
+		}
+		cambiarSrc(songList[prev].url, prev);
+	};
+	const nextSong = () => {
+		let next = songActual + 1;
+		if (next > songList.length - 1) {
+			next = 0;
+		}
+		cambiarSrc(songList[next].url, next);
+	};
+
+	//CAMBIAR SRC DE AUDIO
+
+	const cambiarSrc = (url, song) => {
+		let string = "https://assets.breatheco.de/apis/sound/";
+		audio.current.src = string + url;
+		setSongActual(song);
+		audio.current.play();
+		setPlaying(true);
+	};
 	//RETURN
 	return (
 		<div className="container">
-			<div className="header"></div>
+			<div className="header">
+				<img src="https://www.flaticon.com/svg/vstatic/svg/890/890806.svg?token=exp=1620240149~hmac=0c979ec6a405de5347a11817f65d9b92" />
+			</div>
 			{songList.map((objeto, index) => {
 				return (
-					<div className="song" key={index}>
-						<div onClick={() => {}}>
+					<div
+						className="song"
+						key={index}
+						onClick={() => {
+							cambiarSrc(objeto.url, index);
+						}}>
+						<div>
 							<span>
 								{objeto.id}
 								{" - "}
 							</span>
 							<span>{objeto.name}</span>
 						</div>
-						<audio ref={audio} src={songList.url} controls></audio>
 					</div>
 				);
 			})}
-
+			<audio ref={audio} src={songList.url}></audio>
 			<div className="footer fluid fixed-bottom">
-				<button className="btn  btn-sm p-0">
+				<button className="btn  btn-sm p-0" onClick={prevSong}>
 					<i className="fas fa-caret-square-left fa-2x boton" />
 				</button>
 				<button className="btn  btn-sm p-0" onClick={playPause}>
-					<i className="fas fa-play fa-2x boton" />
+					{isPlaying ? (
+						<i className="fas fa-pause fa-2x boton"></i>
+					) : (
+						<i className="fas fa-play fa-2x boton"></i>
+					)}
 				</button>
-				<button className="btn  btn-sm p-0">
+				<button className="btn  btn-sm p-0" onClick={nextSong}>
 					<i className="fas fa-caret-square-right fa-2x boton" />
 				</button>
 			</div>
